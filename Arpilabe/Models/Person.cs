d50536f983fb8e5ceb8cb939dfa4f450917b9cde
Sandrine Chastaing <sandrine.chastaing@outlook.fr>
2020-11-11 07:50:03 +0100

    création classe Person

---
+++ Arpilabe/Models/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Arpilabe.Models
+{
+    public class Person
+          {
+            public String firstName  { get; set; }
+
+            [Required]
+            public string lastName { get; set; }
+
+        [Required]
+        public string eMail { get; set; }
+
+        [Required]
+        public int phone { get; set; }
+
+        [Required]
+        public String note { get; set; }
+
+        [Required]
+        public String Departement { get; set; }
+
+        [Required]
+        public DateTime dateOfBirth { get; set; }
+
+
+
+        [Required]
+            [Range(minimum: 0.01, maximum: (double)decimal.MaxValue)]
+            public decimal Price { get; set; }
+        }
+}
